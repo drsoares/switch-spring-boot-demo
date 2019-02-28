6780e15d72335db39ba852471bc74b0d0ba877ca
drsoares <diogosoares@ua.pt>
2019-02-28 00:59:06 +0000

    added database integration with flyway

--- src/main/java/pt/switchprogram/controllers/StudentsController.java
+++ src/main/java/pt/switchprogram/controllers/StudentsController.java
@@ -3,6 +3,7 @@ package pt.switchprogram.controllers;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import pt.switchprogram.domain.Course;
 import pt.switchprogram.domain.Student;
 import pt.switchprogram.repositories.StudentRepository;
 
@@ -21,8 +22,10 @@ public class StudentsController {
     }
 
     @GetMapping
-    public Collection<Student> students() {
-        return studentRepository.findAll();
+    public Collection<Student> students(@RequestParam(value = "course", required = false) Course course) {
+        return Optional.ofNullable(course)
+                .map(studentRepository::findStudentByPreviousCourse)
+                .orElse(studentRepository.findAll());
     }
 
     @PostMapping
@@ -38,10 +41,13 @@ public class StudentsController {
         Optional<Student> studentOptional = studentRepository.findById(id);
         if (!studentOptional.isPresent()) {
             return ResponseEntity.notFound().build();
+        } else {
+            Student existingStudent = studentOptional.get();
+            existingStudent.setName(student.getName());
+            existingStudent.setPreviousCourse(student.getPreviousCourse());
+            studentRepository.save(existingStudent);
+            return ResponseEntity.ok(existingStudent);
         }
-        student.setId(id);
-        studentRepository.save(student);
-        return ResponseEntity.ok(student);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/pt/switchprogram/repositories/StudentRepository.java
+++ src/main/java/pt/switchprogram/repositories/StudentRepository.java
@@ -1,7 +1,11 @@
 package pt.switchprogram.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import pt.switchprogram.domain.Course;
 import pt.switchprogram.domain.Student;
 
+import java.util.List;
+
 public interface StudentRepository extends JpaRepository<Student, Long> {
+    List<Student> findStudentByPreviousCourse(Course course);
 }
